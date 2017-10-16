dc0bdca99ad5b0d976790351b10a6cd751ec5034
Zetian <zetianzzt@gmail.com>
2017-10-16 16:14:42 -0400

    complete simple version

--- src/sampling/ltl_sampling_simple.h
+++ src/sampling/ltl_sampling_simple.h
@@ -15,8 +15,8 @@ private:
     double work_space_size_x_ = 100;
     double work_space_size_y_ = 100;
     std::string ltl_formula_;
-    double EPSILON = 6;
-    double RADIUS = 12;
+    double EPSILON_ = 6;
+    double RADIUS_ = 12;
     std::vector<std::vector<double>> path_;
 
     BAStruct ba_;
@@ -28,12 +28,13 @@ private:
     std::vector<int> sample_from_ba(BAStruct buchi, SampleSpace &sample_space);
     void buchi_post (BAStruct &ba, std::vector<int> indep_set);
     bool if_in_region (std::vector<double> state, Region region);
-    std::vector<double> step_from_to (SampleNode parent_sample, std::vector<double> sampled_state, double EPSILON);
+    std::vector<double> step_from_to (SampleNode parent_sample, std::vector<double> sampled_state);
     int step_from_to_buchi (int paraent_ba, std::vector<double> new_sample_state, BAStruct ba, std::map<int, Region> all_interest_regions);
     std::vector<double> sample_state (std::vector<int> ba_act);
 public:
     void read_formula(std::string ltl_formula, std::vector<std::string> buchi_regions, std::vector<int> indep_set);
     void init_workspace(double work_space_size_x,double work_space_size_y);
+    void init_parameter(double EPSILON, double RADIUS);
     void set_interest_region(std::pair <double, double> position_x, std::pair <double, double> position_y, int interest_id);
     void set_obstacle(std::pair <double, double> position_x, std::pair <double, double> position_y);
     void set_init_state(std::vector<double> init_state);
